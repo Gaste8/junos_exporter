30a5dfef51efa083dc6876a3a163212ad8ac9e9b
Jakob Gastinger <jgastinger@anexia-it.com>
2022-07-20 11:27:12 +0200

    change variable name (feedback)

--- bgp/collector.go
+++ bgp/collector.go
@@ -20,7 +20,7 @@ var (
 	inputMessagesDesc      *prometheus.Desc
 	outputMessagesDesc     *prometheus.Desc
 	flapsDesc              *prometheus.Desc
-	prefixLimit            *prometheus.Desc
+	prefixLimitCountDesc   *prometheus.Desc
 )
 
 func init() {
@@ -29,9 +29,10 @@ func init() {
 	inputMessagesDesc = prometheus.NewDesc(prefix+"messages_input_count", "Number of received messages", l, nil)
 	outputMessagesDesc = prometheus.NewDesc(prefix+"messages_output_count", "Number of transmitted messages", l, nil)
 	flapsDesc = prometheus.NewDesc(prefix+"flap_count", "Number of session flaps", l, nil)
-	prefixLimit = prometheus.NewDesc(prefix+"prefix_limit", "prefix-count variable set in prefix-limit", l, nil)
+	prefixLimitCountDesc = prometheus.NewDesc(prefix+"prefix_limit_count", "prefix-count variable set in prefix-limit", l, nil)
 
 	l = append(l, "table")
+
 	receivedPrefixesDesc = prometheus.NewDesc(prefix+"prefixes_received_count", "Number of received prefixes", l, nil)
 	acceptedPrefixesDesc = prometheus.NewDesc(prefix+"prefixes_accepted_count", "Number of accepted prefixes", l, nil)
 	rejectedPrefixesDesc = prometheus.NewDesc(prefix+"prefixes_rejected_count", "Number of rejected prefixes", l, nil)
@@ -64,7 +65,7 @@ func (*bgpCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- inputMessagesDesc
 	ch <- outputMessagesDesc
 	ch <- flapsDesc
-	ch <- prefixLimit
+	ch <- prefixLimitCountDesc
 }
 
 // Collect collects metrics from JunOS
@@ -113,7 +114,7 @@ func (c *bgpCollector) collectForPeer(p BGPPeer, ch chan<- prometheus.Metric, la
 
 	c.collectRIBForPeer(p, ch, l)
 
-	c.collectPrefixLimitForPeer(p, ch, l)
+	c.collectPrefixLimitCountForPeer(p, ch, l)
 }
 
 func (*bgpCollector) collectRIBForPeer(p BGPPeer, ch chan<- prometheus.Metric, labelValues []string) {
@@ -127,6 +128,6 @@ func (*bgpCollector) collectRIBForPeer(p BGPPeer, ch chan<- prometheus.Metric, l
 	}
 }
 
-func (*bgpCollector) collectPrefixLimitForPeer(p BGPPeer, ch chan<- prometheus.Metric, labelValues []string) {
-	ch <- prometheus.MustNewConstMetric(prefixLimit, prometheus.GaugeValue, float64(p.BGPOI.PrefixLimit.PrefixCount), labelValues...)
+func (*bgpCollector) collectPrefixLimitCountForPeer(p BGPPeer, ch chan<- prometheus.Metric, labelValues []string) {
+	ch <- prometheus.MustNewConstMetric(prefixLimitCountDesc, prometheus.GaugeValue, float64(p.BGPOI.PrefixLimit.PrefixCount), labelValues...)
 }
